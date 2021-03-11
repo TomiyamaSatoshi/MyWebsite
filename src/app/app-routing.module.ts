14f7bf22a07271e2f60ef1d0d659962fcb47870d
tomiyamasatoshi <n2m71212@yahoo.co.jp>
2021-03-11 23:07:19 +0900

    add component home ,inquire ,skill and introduce

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { InquireComponent } from './component/inquire/inquire.component';
+import { IntroduceComponent } from './component/introduce/introduce.component';
+import { SkillComponent } from './component/skill/skill.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'introduce', component: IntroduceComponent},
+  { path: 'skill', component: SkillComponent },
+  { path: 'inquire', component: InquireComponent},
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { IntroduceComponent } from './component/introduce/introduce.component';
+import { SkillComponent } from './component/skill/skill.component';
+import { InquireComponent } from './component/inquire/inquire.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    IntroduceComponent,
+    SkillComponent,
+    InquireComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
