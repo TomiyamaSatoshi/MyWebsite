c3e85fb9cda0bfb4a39b160ac81ffbddb8e884d4
tomiyamasatoshi <n2m71212@yahoo.co.jp>
2021-04-11 23:54:44 +0900

    modify skills component and modify any

--- src/app/component/skill/skill.component.ts
+++ src/app/component/skill/skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppError } from 'src/app/common/app-error';
+import { BadRequestError } from 'src/app/common/bad-request-error';
+import { NotFoundError } from 'src/app/common/not-found-error';
 import { Qualification } from 'src/app/models/qualification';
 import { Skill } from 'src/app/models/skill';
 import { QualificationsService } from 'src/app/service/qualifications.service';
@@ -14,6 +17,9 @@ export class SkillComponent implements OnInit {
   skillsList: Skill[];
   qualificationsList: Qualification[];
 
+  isSkills: Boolean = false;
+  isQualifications: Boolean = false;
+
   constructor(
     private skillsService: SkillsService,
     private qualificationsService: QualificationsService
@@ -24,22 +30,63 @@ export class SkillComponent implements OnInit {
     this.getQualifications();
   }
 
+  // 経験スキルのクリック
+  onClickSkills(isSkills) {
+    this.isSkills = !isSkills;
+    this.isQualifications = false;
+  }
+  // 保有資格クリック
+  onClickQualifications(isQualifications) {
+    this.isQualifications = !isQualifications;
+    this.isSkills = false;
+  }
+
+  // 経験スキルのマウスオーバー
+  mouseOverSkills() {
+    this.isSkills = true;
+    this.isQualifications = false;
+  }
+  // 保有資格のマウスオーバー
+  mouseOverQualifications() {
+    this.isQualifications = true;
+    this.isSkills = false;
+  }
+
+  // 経験スキルの取得
   getSkills() {
     this.skillsService.getSkills().subscribe(
       (skills) => {
         console.log("SUCCESS: " + JSON.stringify(skills));
         this.skillsList = skills;
+      },
+      (error: AppError) => {
+        if (error instanceof NotFoundError) {
+          console.log('ERROR: Not Found.');
+        } else if (error instanceof BadRequestError) {
+          console.log('ERROR: Bad Request.');
+        } else {
+          throw error;
+        }
       }
     );
   }
 
+  // 保有資格の取得
   getQualifications() {
     this.qualificationsService.getQualifications().subscribe(
       (qualifications) => {
         console.log("SUCCESS: " + JSON.stringify(qualifications));
         this.qualificationsList = qualifications;
+      },
+      (error: AppError) => {
+        if (error instanceof NotFoundError) {
+          console.log('ERROR: Not Found.');
+        } else if (error instanceof BadRequestError) {
+          console.log('ERROR: Bad Request.');
+        } else {
+          throw error;
+        }
       }
     );
   }
-
 }
